3bff16ef4bd4b01386027c7943512da389f84442
Combatcmd <combatcmd@gmail.com>
2020-05-29 01:27:36 +0600

    chore: prettify homepage component

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
-import { RouterModule } from '@angular/router'
-import { FormsModule } from '@angular/forms'
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
-import { HomepageRoute } from './homepage.route'
-import { HomepageComponent } from './homepage.component'
+import { HomepageRoute } from "./homepage.route";
+import { HomepageComponent } from "./homepage.component";
 
-import { LotsComponent } from './lots/lots.component'
+import { LotsComponent } from "./lots/lots.component";
 
 @NgModule({
   imports: [RouterModule.forChild([HomepageRoute]), FormsModule, CommonModule],
--- src/app/homepage/homepage.route.ts
+++ src/app/homepage/homepage.route.ts
@@ -2,10 +2,9 @@ import { Route } from "@angular/router";
 import { HomepageComponent } from "./homepage.component";
 
 export const HomepageRoute: Route = {
-    path: '',
-    component: HomepageComponent,
-    data: {
-        authorities: [],
-        pageTitle: 'Homepage'
-    }
+  path: "",
+  component: HomepageComponent,
+  data: {
+    pageTitle: "Homepage",
+  },
 };
