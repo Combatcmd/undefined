6cbc1c086da69f4aa1656903c4cf62259972b89b
Combatcmd <combatcmd@gmail.com>
2020-05-29 00:31:02 +0600

    add: vedio instructions modal

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core'
-import { SearchService } from '../shared'
+import { Component, OnInit } from "@angular/core";
+import { NgbModal, NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
+import { SearchService } from "../shared";
 
 @Component({
-  selector: 'homepage',
-  templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss'],
+  selector: "homepage",
+  templateUrl: "./homepage.component.html",
+  styleUrls: ["./homepage.component.scss"],
 })
 export class HomepageComponent implements OnInit {
-  keyword: string
-  result: any[]
+  keyword: string;
+  result: any[];
+  modalRef: NgbModalRef;
 
-  constructor(public searchService: SearchService) {}
+  constructor(public searchService: SearchService, private modal: NgbModal) {}
 
   ngOnInit(): void {}
+
+  openModal(content) {
+    this.modalRef = this.modal.open(content, {
+      windowClass: "modal__instructions",
+      size: "lg",
+    });
+  }
 }
